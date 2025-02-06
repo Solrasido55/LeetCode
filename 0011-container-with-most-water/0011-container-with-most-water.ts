9197738bfab03b3123c0db9237b2868872171fef
Sol Kim <106805946+Solrasido55@users.noreply.github.com>
2025-02-06 12:10:55 +0900

    Time: 3 ms (50.8%), Space: 58.6 MB (98.83%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.ts
@@ -0,0 +1,17 @@
+function maxArea(height: number[]): number {
+    let res = 0;
+    let l = 0;
+    let r = height.length - 1;
+    while (l !== r) {
+        const w = r - l;
+        const lh = height[l];
+        const rh = height[r];
+        const h = lh < rh ? lh : rh;
+        const ext = w * h;
+        if (res < ext) {
+            res = ext;
+        }
+        lh < rh ? l++ : r--;
+    }
+    return res;
+};
