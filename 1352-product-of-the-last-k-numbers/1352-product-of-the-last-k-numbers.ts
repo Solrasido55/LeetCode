7eb0d9411ff451bf6a5ee1ace1a8bdb3a9dce08a
Sol Kim <106805946+Solrasido55@users.noreply.github.com>
2025-02-14 20:01:14 +0900

    Time: 34 ms (100%), Space: 79.5 MB (66.67%) - LeetHub

--- 1352-product-of-the-last-k-numbers/1352-product-of-the-last-k-numbers.ts
+++ 1352-product-of-the-last-k-numbers/1352-product-of-the-last-k-numbers.ts
@@ -1,19 +1,39 @@
 class ProductOfNumbers {
-    private list: number[] = []
-    constructor() {}
+    private list: number[];
+    private size: number;
+    constructor() {
+        this.list = [];
+        this.size = 0;
+    }
 
     add(num: number): void {
-        this.list.push(num);
+        this.size++;
+        switch (true) {
+            case num === 0:
+                this.list.length = 0;
+                break;
+            case this.list.length === 0:
+                this.list.push(num);
+                break;
+            default:
+                const last = this.list[this.list.length - 1];
+                this.list.push(last * num);
+                break;
+        }
         return null;
     }
 
     getProduct(k: number): number {
-        const last:number = this.list.length - 1;
-        let result: number;
-        for (let i = last; i > last - k; i--) {
-            result = (result ?? 1) * this.list[i]
+        const length = this.list.length;
+        const last = length - 1;
+        switch (true) {
+            case k > length:
+                return length === this.size ? this.list[last] : 0
+            case k === length:
+                return this.list[last]
+            default:
+                return this.list[last] / this.list[last - k]
         }
-        return result;
     }
 }
 
