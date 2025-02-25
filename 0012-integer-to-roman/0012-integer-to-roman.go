ed75240d437a959e80d1b9cd800d1697f12fdcee
Sol Kim <106805946+Solrasido55@users.noreply.github.com>
2025-02-25 20:30:54 +0900

    Time: 0 ms (100%), Space: 5 MB (82.52%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.go
+++ 0012-integer-to-roman/0012-integer-to-roman.go
@@ -22,16 +22,15 @@ func convert(num int, roman []string)string {
     return res
 }
 func intToRoman(num int) string {
-    th := (num / 1000) * 1000
-    h := ((num - th) / 100) * 100
-    t := ((num - th - h) / 10) * 10
-    o := num - th - h - t
+    th := num / 1000
+    h := (num % 1000) / 100
+    t := (num % 100) / 10
+    o := num % 10
 
     var roman string
-
-    roman = roman + convert(th / 1000, []string{"M", "", ""})
-    roman = roman + convert(h / 100, []string{"C", "D", "M"})
-    roman = roman + convert(t / 10, []string{"X", "L", "C"})
+    roman = roman + convert(th, []string{"M", "", ""})
+    roman = roman + convert(h, []string{"C", "D", "M"})
+    roman = roman + convert(t, []string{"X", "L", "C"})
     roman = roman + convert(o, []string{"I", "V", "X"})
     return roman
 }
